df368a4918e7a7cf430448dfe72a86a6eadd77d9
Daniel Frascarelli <daniel.frascarelli@frasal.uy>
2025-11-01 15:40:08 -0300

    box-card

--- src/components/box-card/BoxCard.tsx
+++ src/components/box-card/BoxCard.tsx
@@ -32,13 +32,13 @@ export default function BoxCard({
   };
 
   return (
-    <div className={`flat-offset-card ${className}`} style={vars}>
+    <div className={`box-card ${className}`} style={vars}>
       <div className="common-layout-margin d-flex flex-row align-items-center gap-3">
         <div className="d-flex justify-content-center align-items-center flex-shrink-0">
           <img src={imgSrc ?? important} className="img-logo" alt="important" />
         </div>
 
-        <div className="flat-offset-card__content flex-grow-1 mb-0">
+        <div className="box-card__content flex-grow-1 mb-0">
           {children}
         </div>
       </div>
