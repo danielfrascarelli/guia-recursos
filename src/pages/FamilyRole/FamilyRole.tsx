bd67b7e44cd0314902392b336aeab16549cf8a19
Daniel Frascarelli <daniel.frascarelli@frasal.uy>
2025-10-31 22:12:22 -0300

    estilos de FamilyRole

--- src/pages/FamilyRole/FamilyRole.tsx
+++ src/pages/FamilyRole/FamilyRole.tsx
@@ -1,35 +1,39 @@
-import InlineSpotlight from "../../components/inline-spotlight/InlineSpotlight";
 import FpModal from "../../components/modal/FpModal";
-import OffsetCard from "../../components/offset-card/OffsetCard";
 import "./family-role.scss";
 
 import child from "../../assets/images/child.png";
+import FlatOffsetCard from "../../components/flat-offset-card/FlatOffsetCard";
+import BoxCard from "../../components/box-card/BoxCard";
 
 export default function FamilyRole() {
   return (
     <div className="h-100">
-      <div className="mx-3 p-0 pt-3 fp-family-role">
-        <strong> <p>
-          ¿Quienes&nbsp;<InlineSpotlight bg="var(--brand-wine)">acompañamos</InlineSpotlight>&nbsp;los procesos de encarcelamiento?</p>
-        </strong>
-        <p className="text-justify">
-          En su mayoría somos las familiares mujeres, parejas, madres, hermanas quienes asumimos los costos de estos procesos tan complejos, que nos obligan a afrontar los cuidados del adentro y del afuera.
-        </p>
-        <p className="text-justify">
-          <InlineSpotlight bg="var(--brand-wine)">Acompañamos</InlineSpotlight>&nbsp;porque somos agentes de prevención de la tortura. Somos la voz de quién se encuentra cumpliendo una pena, somos la garantía de que se mejoren las condiciones y se respeten los Derechos Humanos.
-        </p>
-        <p className="text-justify">
-          <InlineSpotlight bg="var(--brand-wine)">Sostenemos</InlineSpotlight> porque sabemos que la vida continua afuera, porque la vida continúa luego, porque la vida para ser transformada necesita de esperanza y dignidad.
-        </p>
-        <div className="mx-1 p-0 pt-2 mt-1 fp-tips float-clean">
-          <OffsetCard
+      <div className="fp-family-role">
+        <div className="common-layout-margin">
+          <strong>
+            <p>
+              ¿Quienes acompañamos los procesos de encarcelamiento?
+            </p>
+          </strong>
+          <p className="text-justify">
+            En su mayoría somos las familiares mujeres, parejas, madres, hermanas quienes asumimos los costos de estos procesos tan complejos, que nos obligan a afrontar los cuidados del adentro y del afuera.
+          </p>
+          <p className="text-justify">
+            <strong>Acompañamos</strong> porque somos agentes de prevención de la tortura. Somos la voz de quién se encuentra cumpliendo una pena, somos la garantía de que se mejoren las condiciones y se respeten los Derechos Humanos.
+          </p>
+          <p className="text-justify">
+            <strong>Sostenemos</strong> porque sabemos que la vida continua afuera, porque la vida continúa luego, porque la vida para ser transformada necesita de esperanza y dignidad.
+          </p>
+        </div>
+        <div className="mt-4 mb-4 fp-tips float-clean">
+          <FlatOffsetCard
             borderColor="#e9a15f"
             bgColor="#efcfb273"
             borderWidth={3}
             offsetX={7} offsetY={7}
             overhangX={7} overhangY={7}
           >
-            <div>
+            <div className="common-layout-margin">
               <img src={child} className="child-image" alt="child logo" />
               <p className="text-justify">
                 Tenes que saber que si tu familiar detenido tiene infancias o adolescencias a su cargo,
@@ -41,14 +45,26 @@ export default function FamilyRole() {
                 los/as niños, niñas y adolescentes (NNA).
               </p>
             </div>
-          </OffsetCard>
+          </FlatOffsetCard>
         </div>
         <div className="mx-2 p-0 mt-4 text-font-size lh-base">
           <FpModal title="Si queres saber más acerca de Los principios de Bogota, pincha acá" onClick={() => { window.open('https://rimuf.org/wp-content/uploads/2023/03/Version-oficial-Espanol-Principios-de-Bogota-Bogota-Colombia-2023-.docx.pdf', '_blank', 'noopener,noreferrer'); }} />
+        </div> 
+        <div className="mt-4 mb-4 fp-tips float-clean">
+          <BoxCard
+            borderColor="#e9a15f"
+            bgColor="#efcfb273"
+            borderWidth={3}
+            offsetX={7} offsetY={7}
+            overhangX={7} overhangY={7}
+          >
+            <p className="text-justify">
+              Si tu familiar es una mujer, está detenida y embarazada o
+              tiene hijos/as pequeños. Tiene derecho a ser trasladada a una unidad para
+              madres con hijos/as, donde pueden ingresar los niños y niñas de 0 a 3 años.
+            </p>
+          </BoxCard>
         </div>
-        <p className="text-justify">
-          <InlineSpotlight bg="var(--brand-wine)">Si tu familiar es una mujer</InlineSpotlight>, está detenida y embarazada o tiene hijos/as pequeños. Tiene derecho a ser trasladada a una unidad para madres con hijos/as, donde pueden ingresar los niños y niñas de 0 a 3 años.
-        </p>
       </div>
     </div>
   );
