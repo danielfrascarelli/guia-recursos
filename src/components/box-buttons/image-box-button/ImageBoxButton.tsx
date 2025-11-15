3a5e8c9041f1682c39fdafbb1c825d46416bc7c7
Luciana Prates <lucianaprates10@gmail.com>
2025-11-15 17:16:45 -0300

    cambio botones menu

--- src/components/box-buttons/image-box-button/ImageBoxButton.tsx
+++ src/components/box-buttons/image-box-button/ImageBoxButton.tsx
@@ -1,5 +1,6 @@
 import './image-box-button.scss';
 import type { ImageBoxButtonProps } from './ImageBoxButtonProps';
+import InfoIcon from '../../../../../guia-recursos/src/assets/images/icons/info.svg'; 
 
 export default function ImageBoxButton({
   className = "", 
@@ -12,9 +13,18 @@ export default function ImageBoxButton({
     <div className={`image-box-button__container`} onClick={onClick}>
       <div className={`image-box-button ${className}`} >
         <div className="d-flex flex-row flex-grow-1 align-items-center gap-3">
+
+        {srcImg ? (
           <div className="d-flex justify-content-center align-items-center flex-shrink-0 image-box-button-img-logo__container">
             <img src={srcImg} className="image-box-button-img-logo" alt="important" />
           </div>
+        ) : (
+          <div className="d-flex justify-content-center align-items-center flex-shrink-0 image-box-button-img-logo__container">
+            <img src={InfoIcon} className="image-box-button-img-logo" alt="info" />
+          </div>
+        )}
+
+          
           <div className="d-flex flex-row justify-content-center image-box-button__content flex-grow-1 mb-0">
             {label}
           </div>
