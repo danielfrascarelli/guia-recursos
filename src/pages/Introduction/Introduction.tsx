7d0d6b0f793ca35a24dcfc74b57d2a2b82db62af
Daniel Frascarelli <daniel.frascarelli@frasal.uy>
2025-11-15 16:48:27 -0300

    mejoras estilos introduccion

--- src/pages/Introduction/Introduction.tsx
+++ src/pages/Introduction/Introduction.tsx
@@ -1,5 +1,5 @@
 import WomanBoxButton from "../../components/box-buttons/woman-box-button/WomanBoxButton";
-import FpModal from "../../components/modal/FpModal";
+// import FpModal from "../../components/modal/FpModal";
 
 import "./introduction.scss";
 
@@ -15,20 +15,11 @@ export default function Introduction() {
             <p>Desde <strong>Familias Presentes</strong> creemos que para acompañar en el proceso que realiza nuestro familiar detenido, necesitamos acceder a la información, hacer que circule para generar las mejores garantías en el proceso.</p>
             <p>Realizamos esta guía: para compartir lo que aprendimos acompañando, para que no estés sola/o, para hacer valer nuestros derechos, porque el encarcelamiento también nos hace ser parte. </p>
           </div>
-
-          {/*
-          <div className="mx-2 p-0 text-font-size lh-base">
-            <FpModal title="Acompañar!" modalTitle="Acompañar">
-              La palabra "acompañar" proviene del latín vulgar "companiare", que a su vez deriva de "cum panis" (con pan). Originalmente, significaba compartir el pan, es decir, estar con alguien y compartir un alimento básico, lo que implicaba cercanía y apoyo mutuo. Con el tiempo, el significado se extendió para abarcar la idea de estar con alguien en diferentes situaciones, no solo en la comida, sino también en el camino, en momentos importantes, ofreciendo compañía y apoyo.
-              El origen en "cum panis" (con pan) es clave para entender el significado profundo de acompañar.
-              <p className="color-red"></p>
-            </FpModal>
-          </div>
-          */}
-
-          <div className="mt-4 mb-4 fp-tips float-clean">
+        </div>
+        <div>
+          <div className="mt-4">
             <WomanBoxButton className="fp-introduction-woman-card">
-              <p className="text-justify">
+              <p className="fp-introduction-woman-card-content text-justify">
                 <a href="https://www.oas.org/es/cidh/informes/pdfs/2025/resolucion_ppl-es.pdf" target="_blank">
                   Entrá acá para ver el informe de la CIDH (02/2025) sobre derechos de familiares y afectos de personas privadas de libertad.
                 </a>
@@ -41,11 +32,3 @@ export default function Introduction() {
   );
 }
 
-
-{/* <div className="mx-2 p-0 text-font-size lh-base">
-        <FpModal title="Acompañar!" modalTitle="Acompañar">
-          La palabra "acompañar" proviene del latín vulgar "companiare", que a su vez deriva de "cum panis" (con pan). Originalmente, significaba compartir el pan, es decir, estar con alguien y compartir un alimento básico, lo que implicaba cercanía y apoyo mutuo. Con el tiempo, el significado se extendió para abarcar la idea de estar con alguien en diferentes situaciones, no solo en la comida, sino también en el camino, en momentos importantes, ofreciendo compañía y apoyo.
-          El origen en "cum panis" (con pan) es clave para entender el significado profundo de acompañar.
-          <p className="color-red"></p>
-        </FpModal>
-      </div> */}
