4a9933b0089691fcf8e1154e19333c54213ab92f
Luciana Prates <lucianaprates10@gmail.com>
2025-11-16 15:51:28 -0300

    cambio estructura de unidades

--- src/pages/ContactosUtiles/components/inr/inr-unidad-component/InrUnidadProps.ts
+++ src/pages/ContactosUtiles/components/inr/inr-unidad-component/InrUnidadProps.ts
@@ -1,19 +1,25 @@
-import type { DefinitionProps } from "../../../../../components/DefinitionProps";
-import type { OfficeData } from "../../common/common-component/CommonOfficeInfoProps";
+import type { DefinitionProps } from '../../../../../components/DefinitionProps';
+import type { OfficeData } from '../../common/common-component/CommonOfficeInfoProps';
 
 export interface InfoUnidad extends OfficeData {
-    packages: {
-        deliveryHours: string;
-        restrictionsUrl?: string;
-    };
-    visiting: {
-        visitingHours: string;
-        restrictionsDressCodeUrl?: string;
-        restrictionsText: string[];
-    };
-};
+  packages: {
+    deliveryHours: string;
+    restrictionsUrl?: string;
+  };
+  visiting: {
+    visitingHours: string;
+    visitingRestrictions: string;
+    visitingHoursConjugal: string;
+    visitingRestrictionsConjugal: string;
+    restrictionsDressCodeUrl?: string;
+    //restrictionsText: string[];
+  };
+}
+export interface GeneralInfoUnidad {
+  restrictionsText: string[];
+}
 
 export interface InrUnidadProps extends DefinitionProps {
-    data: InfoUnidad;
-    shortAddressDescription: string;
-};
+  data: InfoUnidad;
+  shortAddressDescription: string;
+}
